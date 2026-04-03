b38e7eea8b8bfc19c7220428b712aa80ffda101e
Khalilullah Turk <ktautomatex@gmail.com>
2026-04-03 21:56:53 +0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import KTAutomateX from './KTAutomateX'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <KTAutomateX />
-  </React.StrictMode>
-)
