7f8f50a83de2d50be6f84eaa71373aa8324069d7
Khalilullah Turk <ktautomatex@gmail.com>
2026-04-03 21:22:05 +0500

    Initialize React application with KTAutomateX component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import KTAutomateX from './KTAutomateX'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <KTAutomateX />
+  </React.StrictMode>
+)
